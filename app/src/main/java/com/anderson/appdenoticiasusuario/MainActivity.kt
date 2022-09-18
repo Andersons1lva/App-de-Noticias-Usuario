7fd1956e5aa0dc46bf221aa23ae8cbf73f2339ee
andersons1lva <andersons1lva@yahoo.com.br>
2022-09-18 12:23:03 -0300

    Create fun recuperarNoticia().

--- app/src/main/java/com/anderson/appdenoticiasusuario/MainActivity.kt
+++ app/src/main/java/com/anderson/appdenoticiasusuario/MainActivity.kt
@@ -3,10 +3,13 @@ package com.anderson.appdenoticiasusuario
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.anderson.appdenoticiasusuario.databinding.ActivityMainBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +19,20 @@ class MainActivity : AppCompatActivity() {
         //Ocultação da Toolbar de serviço.
         supportActionBar!!.hide()
 
+        recuperarNoticia()
+    }
+
+    // função para recuperar os dados servidor
+    private fun recuperarNoticia(){
+        db.collection("noticias").document("noticia").get()
+            .addOnCompleteListener { document ->
+                if (document.isSuccessful){
+                    //recuperação dos campos do banco
+                    val titulo = document.result.get("titulo").toString()
+                    val noticia = document.result.get("noticia").toString()
+                    val data = document.result.get("data").toString()
+                    val autor = document.result.get("autor").toString()
+                }
+            }
     }
 }
