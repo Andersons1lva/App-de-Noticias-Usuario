b05cecbfc155043e369fe5880d590969d207d2af
andersons1lva <andersons1lva@yahoo.com.br>
2022-09-18 12:13:36 -0300

    Setting Database Firebase and Implement Viewbinding.

--- app/src/main/java/com/anderson/appdenoticiasusuario/MainActivity.kt
+++ app/src/main/java/com/anderson/appdenoticiasusuario/MainActivity.kt
@@ -2,10 +2,19 @@ package com.anderson.appdenoticiasusuario
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.anderson.appdenoticiasusuario.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Ocultação da Toolbar de serviço.
+        supportActionBar!!.hide()
+
     }
 }
